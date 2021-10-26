315be3bc2949a9208d6f266085dbe7cc877de287
Vladislav117 <siluetkysta@gmail.com>
2021-10-26 23:22:24 +0300

    Disabling eco and spores modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
-from modules.eco import *
-from modules.spores import *
+# from modules.eco import *
+# from modules.spores import *
 from modules.rp import *
 from modules.voting import *
 from modules.isaak import *
